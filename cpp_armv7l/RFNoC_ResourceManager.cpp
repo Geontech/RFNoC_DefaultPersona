3e5cdaa78c6277f511717317693ee83e4ca3e46c
Patrick Wolfram <pwolfram@geontech.com>
2017-02-20 17:46:11 -0500

    Fixed order of instantiation and adding RFNoC Resource to map.

--- cpp_armv7l/RFNoC_ResourceManager.cpp
+++ cpp_armv7l/RFNoC_ResourceManager.cpp
@@ -63,15 +63,18 @@ Resource_impl* RFNoC_ResourceManager::addResource(int argc, char* argv[], Constr
         return NULL;
     }
 
+    // Map the RFNoC_Resource
+    this->idToResource[resourceID] = rfNocResource;
+
+    // Instantiate the resource
     Resource_impl *resource = rfNocResource->instantiate(argc, argv, fnptr, libraryName);
 
     if (not resource) {
         LOG_ERROR(RFNoC_ResourceManager, "Failed to instantiate REDHAWK Resource");
+        delete rfNocResource;
+        this->idToResource.erase(resourceID);
         return NULL;
     }
-
-    // Map the RFNoC_Resource
-    this->idToResource[resourceID] = rfNocResource;
 
     return resource;
 }
