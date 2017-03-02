44cadf26d130869acd20a66e2ef7f9b3cdb44352
Patrick Wolfram <pwolfram@geontech.com>
2017-03-01 20:28:22 -0500

    Fixed issue with shared pointer.

--- cpp_armv7l/RFNoC_Resource.cpp
+++ cpp_armv7l/RFNoC_Resource.cpp
@@ -66,7 +66,7 @@ void RFNoC_Resource::handleIncomingConnection(const std::string &streamID, const
         uhd::rfnoc::block_ctrl_base::sptr providesBlockPtr = this->resourceManager->getUsrp()->get_block_ctrl(providesBlock.blockID);
 
         if (providesBlockPtr->list_upstream_nodes().count(providesBlock.port)) {
-            if (providesBlockPtr->list_upstream_nodes()[providesBlock.port]->unique_id() == block.blockID) {
+            if (providesBlockPtr->list_upstream_nodes().at(providesBlock.port).lock()->unique_id() == block.blockID) {
                 LOG_DEBUG_ID(RFNoC_Resource, this->ID, "Already connected to block");
                 return;
             }
